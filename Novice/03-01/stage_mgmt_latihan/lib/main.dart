1c489bafc015e37d67d1243c87d7f799d8732662
zakkysykes <muhammad1700016076@webmail.uad.ac.id>
2020-03-26 12:13:09 +0700

    03-o1

---
+++ Novice/03-01/stage_mgmt_latihan/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'theme.dart';
+import 'cart.dart';
+import 'catalog.dart';
+import 'screens/cart.dart';
+import 'screens/catalog.dart';
+import 'screens/login.dart';
+
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+Widget build(BuildContext context) {
+  return MultiProvider(
+    providers: [
+      Provider(create: (context) => CatalogModel()),
+
+      ChangeNotifierProxyProvider<CatalogModel, CartModel>(
+          create: (context) => CartModel(),
+          update: (context, catalog, cart) {
+            cart.catalog = catalog;
+            return cart;
+          },
+        ),
+      ],
+        child: MaterialApp(
+        title: 'Provider Demo',
+        theme: appTheme,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => MyLogin(),
+          '/catalog': (context) => MyCatalog(),
+          '/cart': (context) => MyCart(),
+        },
+      ),
+    );
+  }
+}
